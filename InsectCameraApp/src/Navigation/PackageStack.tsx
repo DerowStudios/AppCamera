c009d24b59ec1084d930df5d700179e4cd125da0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-11-28 22:34:33 -0300

    package list

--- InsectCameraApp/src/Navigation/PackageStack.tsx
+++ InsectCameraApp/src/Navigation/PackageStack.tsx
@@ -10,49 +10,9 @@ function PackageStack() {
     <StackPackage.Navigator>
       <StackPackage.Screen
         name="Inicio"
+        options={{ headerShown: false }}
         component={OpenPackage as any}
-        options={{
-          header: () => (
-            <View style={PlayerHeaderStyles.headerContainer}>
-              {/* Primera columna */}
-              <View style={PlayerHeaderStyles.firstColumn}>
-                <View
-                  style={[
-                    {
-                      borderRadius: 50,
-                      backgroundColor: "white",
-                      width: 90,
-                      height: 90,
-                      justifyContent: "center", // Centrar verticalmente
-                      alignItems: "center", // Centrar horizontalmente
-                    },
-                  ]}
-                >
-                  <Text
-                    style={[
-                      PlayerHeaderStyles.headerText,
-                      { width: 34, color: "black" },
-                    ]}
-                  >
-                    foto
-                  </Text>
-                </View>
-              </View>
-              {/* Segunda columna dividida en dos filas */}
-              <View style={PlayerHeaderStyles.secondColumn}>
-                <View style={PlayerHeaderStyles.secondRow}>
-                  <Text style={PlayerHeaderStyles.headerText}>Fila 1</Text>
-                </View>
-                <View style={PlayerHeaderStyles.secondRow}>
-                  <Text style={PlayerHeaderStyles.headerText}>Fila 2</Text>
-                </View>
-              </View>
-            </View>
-          ),
-          // headerShown: false,
-        }}
       />
-
       <StackPackage.Screen name="Screen1" component={Screen1} />
       <StackPackage.Screen name="Screen2" component={Screen2} />
       <StackPackage.Screen name="Screen3" component={Screen3} />
--- InsectCameraApp/src/Screens/OpenPackage/OpenPackage.tsx
+++ InsectCameraApp/src/Screens/OpenPackage/OpenPackage.tsx
@@ -1,8 +1,8 @@
 import theme from "../../../config/theme";
 import { CameraStackParamList } from "../../Navigation/CameraStack";
 import { ContainerStyles } from "../../Styles";
-import { RouteProp, Text, useEffect, View } from "../../libs";
-import { BackHandler, Pressable, StyleSheet } from "react-native";
+import { RouteProp, Text, useEffect, useState, View } from "../../libs";
+import { BackHandler, FlatList, Pressable, StyleSheet } from "react-native";
 
 type ImageCutRouteProp = RouteProp<CameraStackParamList, "LoadingLayout">;
 
@@ -13,6 +13,29 @@ const OpenPackage = ({
   navigation: any;
   route: ImageCutRouteProp;
 }) => {
+  const [selectedPackage, setSelectedPackage] = useState<number>(0);
+  const paquetes = [
+    "Paquete 1",
+    "Paquete 2",
+    "Paquete 3",
+    "Paquete 4",
+    "Paquete 5",
+    "Paquete 6",
+    "Paquete 7",
+    "Paquete 8",
+    "Paquete 9",
+    "Paquete 10",
+    "Paquete 11",
+    "Paquete 12",
+    "Paquete 13",
+    "Paquete 14",
+    "Paquete 15",
+    "Paquete 16",
+    "Paquete 17",
+    "Paquete 18",
+    "Paquete 19",
+    "Paquete 20",
+  ];
   useEffect(() => {
     const handleBackPress = () => {
       // Navegar a la pantalla "Home"
@@ -30,16 +53,39 @@ const OpenPackage = ({
   const handleRedirect = () => {
     navigation.navigate("Inicio");
   };
+
   return (
     <View style={ContainerStyles.container}>
-      <View style={styles.cards}>
-        <Text>Aca van las cards</Text>
+      <View style={styles.list}>
+        <FlatList
+          data={paquetes}
+          renderItem={({ item, index }) => (
+            <Pressable
+              style={[
+                styles.package,
+                {
+                  left: index * 20, // Desplazamiento horizontal para superposición
+                  zIndex: paquetes.length - index, // Control de la superposición
+                },
+              ]}
+              onPress={() => setSelectedPackage(index)}
+            >
+              <Text style={styles.packageText}>{item}</Text>
+            </Pressable>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+          horizontal={true} // Para permitir "amontonamiento" (columnas)
+          showsHorizontalScrollIndicator
+          contentContainerStyle={{ width: paquetes.length * 23 }} // Estilo del contenedor
+        />
       </View>
-      {/* <Pressable onPress={handleRedirect}>
-        <Text style={{ padding: 20, backgroundColor: "black", color: "white" }}>
-          Go Home
-        </Text>
-      </Pressable> */}
+      <View style={styles.cards}>
+        {selectedPackage !== null && (
+          <View style={styles.card}>
+            <Text style={styles.cardData}>{paquetes[selectedPackage]}</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 };
@@ -52,9 +98,41 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.secondary,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 20,
+    borderRadius: 10,
     height: 300,
     maxWidth: 300,
     minWidth: 300,
+  },
+  card: {
+    width: 200,
+    height: 260,
+    backgroundColor: theme.colors.black,
+  },
+  cardData: {
+    color: "white",
+  },
+  // listContainer: {
+  //   width: paquetes.length * 80,
+  // },
+  list: {
+    flexDirection: "row",
+    height: 100,
+    width: 280,
+    marginBottom: 40,
+    marginTop: -40,
+  },
+  package: {
+    borderWidth: 2,
+    borderColor: "black",
+    position: "absolute",
+    backgroundColor: "#e0e0e0",
+    height: 98,
+    width: 80,
+    borderRadius: 8,
+  },
+  packageText: {
+    color: "#333",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
